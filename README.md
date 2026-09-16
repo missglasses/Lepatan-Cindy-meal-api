fb0e1faca4bad1be49de604e5cb47773176a44c5
missglasses <cinsophie2023@gmail.com>
2026-09-16 19:07:36 +0800

    initial files

--- README.md
+++ README.md
@@ -1,11 +1,2 @@
 # Meal-Plan
 
-data: json
-
-can retrieve data 
-
--text box and search btn when an id is searched (ex: id 3) , display only row 3
-
--load menu btn
-
-
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+const menu = [
+  { id: 1, food: "adobo", price: 85 },
+  { id: 2, food: "fried chicken", price: 95 },
+  { id: 3, food: "yum burger", price: 60 },
+  { id: 4, food: "pomodoro", price: 200 },
+  { id: 5, food: "croissant", price: 150 },
+  { id: 6, food: "cookie", price: 80 },
+  { id: 7, food: "iced coffee", price: 150 },
+  { id: 8, food: "matcha", price: 170 }
+];
+
+// all
+app.get("/api/menu", (req, res) => {
+  res.json(menu);
+});
+
+// indiv (search by id)
+app.get("/api/menu/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const item = menu.find(m => m.id === id);
+
+  if (!item) {
+    return res.status(404).json({ error: "Item not found" });
+  }
+
+  res.json(item);
+});
+
+app.use(express.static(__dirname));
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
